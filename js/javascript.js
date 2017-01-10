61fae9fad60bb8725f64cf970a281fa76584e5ab
Emilie Leterme <emilie.leterme@gmail.com>
2017-01-10 10:28:21 +0100

    Affichage au choix de l'utilisateur

--- js/javascript.js
+++ js/javascript.js
@@ -4,11 +4,27 @@
 //var newLiText = document.createTextNode(nombre);          //Créer du texte à l'intérieur de la variable//
 //newLi.appendChild(newLiText);                            //Met le texte de la variable dans le li//
 
-var nombre;
+//////////////////////////////////////////////Etape 3//////////////////////////////////////////////////////////////////////
 
-for(nombre=0; nombre<=200; nombre++) {
+//var nombre;                                                //Variable qui me permet de changer le texte de chaque case//
+
+//for(nombre=0; nombre<=200; nombre++) {                     //Boucle pour ajouter une case à chaque fois, de O à 200//
+	//var newLi = document.createElement('li');
+	//var newLiText = document.createTextNode(nombre);
+	//document.getElementById('liste').appendChild(newLi);
+	//newLi.appendChild(newLiText);  
+//}
+
+//////////////////////////////////////////////Etape 4////////////////////////////////////////////////////////////////////////
+
+var nombreCase= parseInt(prompt('Quel nombre de case souhaitez-vous afficher?'));     //Affiche la question à l'utilisateur et affecte la réponse à la variable en s'assurant que ce soit un nombre//
+var nombre;
+nombre=1;
+while (nombre <= nombreCase) {
 	var newLi = document.createElement('li');
 	var newLiText = document.createTextNode(nombre);
 	document.getElementById('liste').appendChild(newLi);
-	newLi.appendChild(newLiText);  
+	newLi.appendChild(newLiText);
+	nombre++; 
 }
+
